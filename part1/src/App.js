6c980e4e2c4c7305a0d3a7e0561c9ac435ef1410
Reuben.Frith <reubenfrith007@gmail.com>
2023-07-24 16:10:23 +1000

    part 1 started

---
+++ part1/src/App.js
@@ -0,0 +1,35 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  const friends = [ 'Peter', 'Maya'].join(' and ')
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+      <p>{friends}</p>
+      <Footer />
+    </>
+  )
+}
+export default App;
